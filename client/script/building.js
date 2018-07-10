f1078ecafe640eb2753092343391d2c71c0746b9
Nanaki <nanaki.god@gmail.com>
2018-07-10 15:23:22 +0300

    fix road update building add grid

--- client/script/building.js
+++ client/script/building.js
@@ -2,6 +2,54 @@ window.BUILDING = {
 
     init: callback => {
 
+        TOOL.getJSON('/data/grid.json', data => {
+
+
+            for (let i = 0; i < data[0].length; i++) for (let j = 0; j < data[1].length; j++) {
+
+                let x = 250 * data[0][i];
+                let y = 250 * data[1][j];
+                let isRoadOver = false;
+
+                ROAD.data.forEach(road => {if (road[1] === x && road[2] === y - 250) isRoadOver = true;});
+
+                let building = 'building_' + TEXTURE.building.store[TOOL.getRandomInt(TEXTURE.building.store.length - 1)];
+
+                if (isRoadOver) {
+
+                    let down = new PIXI.Sprite(TEXTURE[building + '_down']);
+
+                    down.anchor.set(0, 1);
+                    down.position.set(x, y + 250);
+
+                    LAYER.building.down.addChild(down);
+
+                    let up = new PIXI.Sprite(TEXTURE[building + '_up']);
+
+                    up.anchor.set(0, 1);
+                    up.position.set(x, y);
+
+                    LAYER.building.up.addChild(up);
+
+                } else {
+
+                    let sprite = new PIXI.Sprite(TEXTURE[building]);
+
+                    sprite.anchor.set(0, 1);
+                    sprite.position.set(x, y + 250);
+
+                    LAYER.building.down.addChild(sprite);
+
+                }
+
+            }
+
+
+            callback();
+
+        });
+
+        /*
         TOOL.getJSON('/data/building.json', data => {
 
             BUILDING.data = data;
@@ -44,6 +92,7 @@ window.BUILDING = {
             callback();
 
         });
+        */
 
     }
 
